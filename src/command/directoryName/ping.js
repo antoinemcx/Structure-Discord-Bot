16fb7564ac95af4676cf7b490aac34510dd3f352
Nandu <89532571+NanduWasTaken@users.noreply.github.com>
2022-02-11 09:52:35 +0530

    Update ping.js

--- src/command/directoryName/ping.js
+++ src/command/directoryName/ping.js
@@ -18,7 +18,7 @@ module.exports = {
 
     run :async (client, message, args) => {   
 
-message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+message.reply(`Pong`);
 
   }
 
