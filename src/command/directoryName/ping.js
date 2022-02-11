4d9c525d7fda5194d8dbafe5953832a4aa653276
Nandu <89532571+NanduWasTaken@users.noreply.github.com>
2022-02-11 09:30:38 +0530

    Update ping.js

--- src/command/directoryName/ping.js
+++ src/command/directoryName/ping.js
@@ -1,3 +1,24 @@
+module.exports = {
+
+    name: 'commandName',
+
+    description: 'commandDescription',
+
+    usage: '<prefix>commandUsage',
+
+    aliases: ['commandAlias1', "commandAlias2"],
+
+    dir: "directoryName",
+
+    cooldown: 1, // Cooldown in seconds, by default it's 2 seconds | OPTIONAL
+
+    permissions: ['permission1', 'permission2'], // OPTIONAL
+
+    
+
+    run :async (client, message, args) => {   
+
+
 if (message.author.bot) return;
 
         const SayMessage = message.content.slice(4).trim();
@@ -8,4 +29,4 @@ if (message.author.bot) return;
 
     }
 
-});
+};
